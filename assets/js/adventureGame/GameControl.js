a51f795fe5a006b05045161f2f1c3924fd0eeb3d
AkhilKulkarni123 <akhilkulki1113@gmail.com>
2025-02-23 22:07:11 -0800

    Update GameControl.js

--- assets/js/adventureGame/GameControl.js
+++ assets/js/adventureGame/GameControl.js
@@ -5,6 +5,8 @@ import GameLevelCity from './GameLevelCity.js';
 import GameLevelMagic from './GameLevelMagic.js';
 import { getStats } from "./StatsManager.js";
 
+
+
 const createStatsUI = () => {
     const statsContainer = document.createElement('div');
     statsContainer.id = 'stats-container';
@@ -23,15 +25,31 @@ const createStatsUI = () => {
     document.body.appendChild(statsContainer);
 };
 
+/**
+ * The GameControl object manages the game.
+ * 
+ * This code uses the JavaScript "object literal pattern" which is nice for centralizing control logic.
+ * 
+ * The object literal pattern is a simple way to create singleton objects in JavaScript.
+ * It allows for easy grouping of related functions and properties, making the code more organized and readable.
+ * In the context of GameControl, this pattern helps centralize the game's control logic, 
+ * making it easier to manage game states, handle events, and maintain the overall flow of the game.
+ * 
+ * @type {Object}
+ * @property {Player} turtle - The player object.
+ * @property {Player} fish
+ * @property {Player} lumberjack
+ * @property {function} start - Initialize game assets and start the game loop.
+ * @property {function} gameLoop - The game loop.
+ * @property {function} resize - Resize the canvas and player object when the window is resized.
+ */
 const GameControl = {
-    intervalID: null,
+    intervalID: null, // Variable to hold the timer interval reference
     localStorageTimeKey: "localTimes",
     currentPass: 0,
     currentLevelIndex: 0,
     levelClasses: [],
     path: '',
-    npcCollisionCount: 0, // Track the number of NPC collisions
-    requiredCollisions: 3, // Set the required collisions to proceed to next level
 
     start: function(path) {
         GameEnv.create();
@@ -41,7 +59,7 @@ const GameControl = {
         this.addExitKeyListener();
         this.loadLevel();
     },
-
+    
     loadLevel: function() {
         if (this.currentLevelIndex >= this.levelClasses.length) {
             this.stopTimer();
@@ -50,12 +68,11 @@ const GameControl = {
         GameEnv.continueLevel = true;
         GameEnv.gameObjects = [];
         this.currentPass = 0;
-        this.npcCollisionCount = 0; // Reset the NPC collision count for the new level
         const LevelClass = this.levelClasses[this.currentLevelIndex];
         const levelInstance = new LevelClass(this.path);
         this.loadLevelObjects(levelInstance);
     },
-
+    
     loadLevelObjects: function(gameInstance) {
         this.initStatsUI();
         // Instantiate the game objects
@@ -74,29 +91,22 @@ const GameControl = {
             this.handleLevelEnd();
             return;
         }
-
         // Nominal case: update the game objects 
         GameEnv.clear();
         for (let object of GameEnv.gameObjects) {
             object.update();  // Update the game objects
         }
         this.handleLevelStart();
-
-        // Check if the NPC collision condition is met for the City level
-        if (this.currentLevelIndex === 2 && this.npcCollisionCount >= this.requiredCollisions) {
-            this.handleLevelEnd();
-        } else if (this.currentLevelIndex !== 2) {
-            this.handleLevelEnd(); // For other levels, handle the level end as normal
-        }
-
         // Recursively call this function at animation frame rate
         requestAnimationFrame(this.gameLoop.bind(this));
     },
 
     handleLevelStart: function() {
+        // First time message for level 0, delay 10 passes
         if (this.currentLevelIndex === 0 && this.currentPass === 10) {
             alert("Start Level.");
         }
+        // Recursion tracker
         this.currentPass++;
     },
 
@@ -107,95 +117,142 @@ const GameControl = {
         } else { // All levels completed
             alert("Game over. All levels completed.");
         }
+        // Tear down the game environment
         for (let index = GameEnv.gameObjects.length - 1; index >= 0; index--) {
             GameEnv.gameObjects[index].destroy();
         }
+        // Move to the next level
         this.currentLevelIndex++;
+        // Go back to the loadLevel function
         this.loadLevel();
     },
-
+    
     resize: function() {
+        // Resize the game environment
         GameEnv.resize();
+        // Resize the game objects
         for (let object of GameEnv.gameObjects) {
-            object.resize();
+            object.resize(); // Resize the game objects
         }
     },
 
     addExitKeyListener: function() {
         document.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') {
-                // Prevent progression if the NPC collision count hasn't been reached in the City level
-                if (this.currentLevelIndex === 2 && this.npcCollisionCount < this.requiredCollisions) {
-                    alert("You must collide with the NPC more times to proceed.");
-                    return;
-                }
                 GameEnv.continueLevel = false;
             }
         });
     },
 
-    updateTimer: function() {
-        const time = GameEnv.time;
+    /**
+     * Updates and displays the game timer.
+     * @function updateTimer
+     * @memberof GameControl
+     */ 
+    saveTime(time, score) {
+        if (time == 0) return;
+        const userID = GameEnv.userID
+        const oldTable = this.getAllTimes()
 
-        if (GameEnv.timerActive) {
-            const newTime = time + GameEnv.timerInterval;
-            GameEnv.time = newTime;
-            if (document.getElementById('timeScore')) {
-                document.getElementById('timeScore').textContent = (time / 1000).toFixed(2);
-            }
-            return newTime;
+        const data = {
+            userID: userID,
+            time: time,
+            score: score
         }
 
-        if (document.getElementById('timeScore')) {
-            document.getElementById('timeScore').textContent = (time / 1000).toFixed(2);
-        }
-    },
-
-    startTimer: function() {
-        if (GameEnv.timerActive) {
-            console.warn("TIMER ACTIVE: TRUE, TIMER NOT STARTED");
+        if (!oldTable) {
+            localStorage.setItem(this.localStorageTimeKey, JSON.stringify([data]))
             return;
         }
 
+        oldTable.push(data)
+
+        localStorage.setItem(this.localStorageTimeKey, JSON.stringify(oldTable))
+    },
+    getAllTimes() {
+        let timeTable = null;
+
+        try {
+            timeTable = localStorage.getItem(this.localStorageTimeKey);
+        }
+        catch (e) {
+            return e;
+        }
+
+        return JSON.parse(timeTable)
+    },
+    updateTimer() {
+        const time = GameEnv.time
+
+        if (GameEnv.timerActive) {
+            const newTime = time + GameEnv.timerInterval
+            GameEnv.time = newTime                
+            if (document.getElementById('timeScore')) {
+                document.getElementById('timeScore').textContent = (time/1000).toFixed(2) 
+            }
+                return newTime
+            }
+            if (document.getElementById('timeScore')) {
+                document.getElementById('timeScore').textContent = (time/1000).toFixed(2) 
+            }
+    },   
+    /**
+     * Starts the game timer.
+     * @function startTimer
+     * @memberof GameControl
+     */
+    startTimer() {
+        if (GameEnv.timerActive) {
+            console.warn("TIMER ACTIVE: TRUE, TIMER NOT STARTED")
+            return;
+        }
+        
         this.intervalId = setInterval(() => this.updateTimer(), GameEnv.timerInterval);
         GameEnv.timerActive = true;
     },
 
-    stopTimer: function() {
+    /**
+     * Stops the game timer.
+     * @function stopTimer
+     * @memberof GameControl
+     */
+    stopTimer() {   
         if (!GameEnv.timerActive) return;
+        
+        this.saveTime(GameEnv.time, GameEnv.coinScore)
 
-        this.saveTime(GameEnv.time, GameEnv.coinScore);
-
-        GameEnv.timerActive = false;
+        GameEnv.timerActive = false
         GameEnv.time = 0;
         GameEnv.coinScore = 0;
-        this.updateCoinDisplay();
-        clearInterval(this.intervalID);
+        this.updateCoinDisplay()
+        clearInterval(this.intervalID)
     },
 
-    saveTime: function() {
+    saveTime() {
         const data = {
             userID: GameEnv.userID,
             time: GameEnv.time - 10,
             coinScore: GameEnv.coinScore
-        };
+        }
 
-        const currDataList = JSON.parse(localStorage.getItem(this.localStorageTimeKey));
+        const currDataList = JSON.parse(localStorage.getItem(this.localStorageTimeKey))
 
         if (!currDataList || !Array.isArray(currDataList)) {
-            localStorage.setItem(this.localStorageTimeKey, JSON.stringify([data]));
+            localStorage.setItem(this.localStorageTimeKey, JSON.stringify([data]))
             return;
         }
 
-        currDataList.push(data);
-        localStorage.setItem(this.localStorageTimeKey, JSON.stringify(currDataList));
-    },
+        currDataList.push(data)
+        
+        localStorage.setItem(this.localStorageTimeKey, JSON.stringify(currDataList))
+    },  
 
+    // Initialize UI for game stats
     initStatsUI: function() {
         const statsContainer = document.createElement('div');
         statsContainer.id = 'stats-container';
         statsContainer.style.position = 'fixed';
-        statsContainer.style.top = '75px';
+        statsContainer.style.top = '75px'; 
         statsContainer.style.right = '10px';
         statsContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
         statsContainer.style.color = 'white';
@@ -209,15 +266,10 @@ const GameControl = {
         document.body.appendChild(statsContainer);
     },
 
-    // Function to increment collision count (this should be called when the player collides with an NPC)
-    incrementNpcCollisionCount: function() {
-        if (this.currentLevelIndex === 2) { // Only track collisions in the City level
-            this.npcCollisionCount++;
-        }
-    },
 };
 
 // Detect window resize events and call the resize function.
 window.addEventListener('resize', GameControl.resize.bind(GameControl));
 
 export default GameControl;
+
