eabfa54cd18d5b8972c0f5a54743ec5f16123d83
jm1021 <jmort1021@gmail.com>
2026-03-12 12:13:47 -0700

    fix

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from api.titanic import titanic_api
 from model.titanic import initTitanic
 
 app = Flask(__name__)
+
+# Enable CORS for deployed site compatibility
+CORS(app)
 
 # register the titanic api
 app.register_blueprint(titanic_api)
